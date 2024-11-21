13a7294a93ac5c627360e7c00460c6e5e0be7b31
Daniel Bermudez <josedbr18.respaldo@gmail.com>
2024-11-21 13:54:43 -0600

    added /api path to my index route

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/api', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
