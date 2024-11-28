4e11332c5fbc0f759d0c79fc4c97624ec114412a
Daniel Bermudez <josedbr18.respaldo@gmail.com>
2024-11-27 23:03:47 -0600

    sending updated user on phone number route

--- config/googleAuth.js
+++ config/googleAuth.js
@@ -5,7 +5,7 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: `${process.env.BACKEND_HOST}/api/auth/callback`
+    callbackURL: `http://localhost:3200/api/auth/callback`
 },
     function (accessToken, refreshToken, profile, cb) {
         User.findOne({ provider_id: profile.id })
