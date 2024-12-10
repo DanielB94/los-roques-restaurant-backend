31a24f61091a6427d4dc7e840d3a0bbce113400e
Daniel Bermudez <josedbr18.respaldo@gmail.com>
2024-12-09 21:03:16 -0600

    reversing all the changes to try another approach

--- config/socketConfig.js
+++ config/socketConfig.js
@@ -31,8 +31,8 @@ function initializeSocket(server) {
           Order.watch().on('change', async (change)=>{
             console.log('Something has changed');
             console.log(change.documentKey._id);
-            const order = await Order.find(change.documentKey._id);
-            if(order.paid !== false || order.status === false) {
+            const order = await Order.findById(change.documentKey._id);
+            if(order.paid !== false || order.status !== false) {
             io.to('AdminRoom').emit('changes', order);
             }
           });
